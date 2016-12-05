2eb6b507a03e7b1afff9d5550a46becfe2b15d4b
JRLi <fanicesiza@gmail.com>
2016-12-05 19:50:44 +0800

    test

--- ForTrain/getMysql.py
+++ ForTrain/getMysql.py
@@ -1,25 +1,45 @@
 # -*- coding: UTF-8 -*-
 import pymysql.cursors
-
+import csv
 '''
 import sys
 accountName = str(sys.argv[1])
 passWd = str(sys.argv[2])
 '''
 tableName = 'Standard_Diviation_15'
-inputData = open("D:/Project/platelet/db.txt", "r")
-for line in inputData.readlines():
-    stringField = line.replace("\n", "").split("\t")
-    accountName = stringField[0]
-    passWd = stringField[1]
-inputData.close()
-conn = pymysql.connect(host='jdbc:mysql://140.120.203.44', user=accountName, password=passWd, db='siza',
-                       charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
+dbName = 'siza'
+
+with open("D:/Project/platelet/db.txt", "r") as inputData:
+    for line in inputData:
+        stringField = line.replace("\n", "").split("\t")
+        accountName = stringField[0]
+        passWd = stringField[1]
+
+conn = pymysql.connect(host='140.120.203.44', user=accountName, password=passWd, db=dbName,
+                       charset='utf8', cursorclass=pymysql.cursors.Cursor)  # if dict out: pymysql.cursors.DictCursor
+sql = 'SELECT * FROM ' + tableName
 
 curData = conn.cursor()
-sql = 'SELECT * FROM '+tableName
 curData.execute(sql)
+
 print(curData.description)
+field_names = [i[0] for i in curData.description]   # if need to get column name
+print(field_names)
+print(type(curData))    # tuple
+
+result = curData.fetchall()
+print('result type:', type(result))
+print(result[1:2:])
+testA = result[1]
+print(testA, type(testA))
+print(testA[1])
+with open('E:/StringTemp/sqlTest2.csv', 'w') as fp:
+    for i in field_names:       # if need to write column name
+        fp.write(i + ",")
+    fp.write("\n")
+    myFile = csv.writer(fp, lineterminator='\n')
+    myFile.writerows(result)
 
 curData.close()
 conn.close()
+
