4304f03fe0486433a2b3160acd2d9fbe760f8bb6
JRLi <fanicesiza@gmail.com>
2016-12-09 22:13:31 +0800

    test

--- ForReference/mltest.py
+++ ForReference/mltest.py
@@ -4,16 +4,20 @@ from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+
 file_path = 'D:/Project/python_learning/games.csv'
 # Read in the data.
 games = pd.read_csv(file_path)
 # Print the names of the columns in games.
 print(games.columns)
 print(games.shape)
+
 # Make a histogram of all the rating in the average_rating column.
 plt.hist(games["average_rating"])
 # Show the plot.
 # plt.show()
+
 # Print the first row of all the games with zero scores.
 # The .iloc methods on the data frames allows us to index by position.
 print(games[games["average_rating"] == 0].iloc[0])
@@ -52,7 +56,7 @@ print(games.corr()["average_rating"])
 # Get all the columns from the dataframe.
 columns1 = games.columns.tolist()
 # Filter the columns to remove ones we don't want.
-columns1 = [c for c in columns1 if c not in ["bayes_average_rating", "average_rating", "type", "names"]]
+columns1 = [c for c in columns1 if c not in ["bayes_average_rating", "average_rating", "type", "name"]]
 # Store the variable we'll be predicting on.
 target = "average_rating"
 print(columns1)
@@ -70,6 +74,19 @@ train, test = train_test_split(games, train_size=0.8, random_state=1)
 print(test.shape, train.shape)
 
 # Fitting a linear regression
+# Initialize the model class.
+model1 = LinearRegression()
+model2 = LinearRegression()
+# Fit the model to the training data
+model1.fit(training[columns1], training[target])
+model2.fit(train[columns1], train[target])
 
-
+# Predicting error
+# Generate out predictions for the test set.
+predictions1 = model1.predict(testing[columns1])
+predictions2 = model2.predict(test[columns1])
+# Compute error between our test predictions and the actual values.
+mse1 = mean_squared_error(predictions1, testing[target])
+mse2 = mean_squared_error(predictions2, test[target])
+print(mse1, mse2)
 
