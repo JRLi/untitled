db3c45d77905ce1c939de529fe53d88d77de0429
JRLi <fanicesiza@gmail.com>
2016-12-23 22:59:55 +0800

    from 36

--- QuickService/fastaqcr.py
+++ QuickService/fastaqcr.py
@@ -22,7 +22,8 @@ def args_parse():
     subparsers = parser.add_subparsers(help='choice file format', dest='command')
 
     parser_a = subparsers.add_parser('fa', help='input is fasta format')
-    parser_a.add_argument('-b', '--base', type=int, default=60, help='base pairs per line; if set 0, all in one line')
+    # parser_a.add_argument('-b', '--base', type=int, default=60, help='base pairs per line; if set 0, all in one line')
+    parser_a.add_argument('-b', '--base', type=int, help='base pairs per line; if set 0, all in one line')
     parser_a.add_argument('-i', '--id2seq', action="store_true", help="output ID2Sequence file, separated by \\t")
     parser_a.add_argument('input', nargs='+', help="input fasta file")
     parser_a.add_argument('-s', '--split', type=int, default=1, help='split fasta file two [-s] parts')
@@ -46,32 +47,50 @@ class Usage(Exception):
 
 
 def fa_parser(file, base, rev):
-    with open('./' + file, 'r') as inputFile:
+    with open(file, 'r') as inputFile:
         id2seq_dict = OrderedDict()
-        id, seq = '', []
-
+        id, seq, total_base, total_gc, len_list, check_len, n50, n90 = '', [], 0, 0, [], 0, [], []
         for line in inputFile:
             if line.startswith('>'):
                 if id != '':
                     fseq = reverse_complement(''.join(seq), True) if rev else ''.join(seq)
-                    bfseq = fseq + '\n' if base == 0 else insert_end(fseq, base)
+                    total_base += len(fseq)
+                    len_list.append(len(fseq))
+                    total_gc += gc_base(fseq)
+                    bfseq = fseq + '\n' if base in (0, None) else insert_end(fseq, base)
                     id2seq_dict[id] = bfseq
                 id = line.replace('\"', '').replace('\r\n','').replace('\n', '')[1:]
-                print(id)
                 seq = []
             else:
                 seq.append(line.replace('\r\n','').replace('\n', ''))
-
         fseq = reverse_complement(''.join(seq), True) if rev else ''.join(seq)
-        bfseq = fseq + '\n' if base == 0 else insert_end(fseq, base)
+        total_base += len(fseq)
+        len_list.append(len(fseq))
+        total_gc += gc_base(fseq)
+        bfseq = fseq + '\n' if base in (0, None) else insert_end(fseq, base)
         id2seq_dict[id] = bfseq
-        return id2seq_dict
+        len_list.sort(reverse=True)
+        max_contig, min_contig = len_list[0], len_list[-1]
+        gc_content = total_gc/total_base
+        total_contig = len(id2seq_dict)
+        for j in len_list:
+            check_len += j
+            if check_len >= total_base/2:
+                n50.append(j)
+            if check_len >= total_base*9/10:
+                n90.append(j)
+        return id2seq_dict, total_contig, max_contig, min_contig, n50[0], n90[0], total_base, gc_content
 
 
-def dict2file(dictIn, pathAndName):
-    with open(pathAndName, 'w') as inputFile:
-        for k, v in dictIn.items():
-            inputFile.write(k + '\t' + v.replace('\r\n','').replace('\n', '') + '\n')
+def dict2file(dictIn, arg_id2seq, base_number, attlist):
+    if arg_id2seq:
+        with open(attlist[0] + attlist[1] + attlist[3] + '.id2seq', 'w') as inputFile:
+            for k, v in dictIn.items():
+                inputFile.write(k + '\t' + v.replace('\r\n','').replace('\n', '') + '\n')
+    if base_number is not None:
+        with open(attlist[0] + attlist[1] + '.' + str(base_number) + attlist[3] + attlist[2], 'w') as outFa:
+            for id in dictIn.keys():
+                outFa.write('>' + id + '\n' + dictIn[id])
 
 
 def reverse_complement(line, seq = False):
@@ -132,15 +151,15 @@ def main(argv=None):
             elif argv.command == 'fa':
                 print('Implement fasta mode.')
                 print(argv)
-                for fileName in argv.input:
-                    fbase, fext = os.path.splitext(fileName)
-                    with open('./' + fbase + '_' + str(argv.base) + root + fext, 'w') as outFa:
-                        id2Seq_dict = fa_parser(fileName, argv.base, argv.reverse)
-                        if argv.id2seq:
-                            dict2file(id2Seq_dict, './' + fbase + '_id2seq')
-                        for id in id2Seq_dict.keys():
-                            outFa.write('>' + id + '\n' + id2Seq_dict[id])
-
+                with open('./SummaryOfFasta.txt', 'w') as rs:
+                    for fileName in argv.input:
+                        fpath, fname = os.path.split(fileName)
+                        fbase, fext = os.path.splitext(fname)
+                        id2Seq_dict, tc, mxc, mic, n50, n90, tb, gcc = fa_parser(fileName, argv.base, argv.reverse)
+                        if argv.id2seq is not None or argv.base is not None:
+                            dict2file(id2Seq_dict, argv.id2seq, argv.base, [fpath, fbase, fext, root])
+                        rs.write('File: {}\nTotal_contigs: {}\nMax_contig: {}\nMin_contig: {}\nn50: {}\nn90: {}\n'
+                                 'Total_bases: {}\nGC_content: {}\n\n'.format(fname, tc, mxc, mic, n50, n90, tb, gcc))
             else:
                 print('Implement fastq mode.')
                 print(argv)
