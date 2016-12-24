b8552b100933fc469edd44c7396c4a46472c0236
JRLi <fanicesiza@gmail.com>
2016-12-24 21:51:25 +0800

    test

--- QuickService/lasooPBMC.py
+++ QuickService/lasooPBMC.py
@@ -9,14 +9,14 @@ import os
 file_path = 'D:/Project/PBMC/logistic_in/'
 out_path = 'D:/Project/PBMC/logistic_out/'
 file_list = os.listdir(file_path)
-print(file_list)
+print('File_list:', file_list)
 # file_path = 'D:/Rworkspace/GSE19301MCLSL'
 
 for file_name in file_list:
     df = pd.read_table(file_path + file_name, index_col=0)
     target = 'Disease'
     cells = df.columns.tolist()
-    print(cells)
+    print(len(cells), cells[0], cells[-1])
     train_score_dict, test_score_dict, test_fscore_dict = {}, {}, {}
     reg = linear_model.Lasso(alpha=0.1)
 
