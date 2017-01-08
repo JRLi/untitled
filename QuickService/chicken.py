8b3446153ae82c586f670b5df5dc5e9c37eb5093
JRLi <fanicesiza@gmail.com>
2017-01-08 19:21:55 +0800

    test

--- QuickService/chicken.py
+++ QuickService/chicken.py
@@ -10,7 +10,7 @@ start_site = int(sys.argv[3])
 end_site = int(sys.argv[4])
 id, seq = None, []
 id2seq_dict = {}
-with open('./'+sequence_file, 'r') as input_file, open('./'+sys.argv[2]+sys.argv[3]+'_'+sys.argv[4]+'.fa', 'w') as out:
+with open('./'+sequence_file, 'r') as input_file, open('./'+sys.argv[2]+'_'+sys.argv[3]+'_'+sys.argv[4]+'.fa', 'w') as out:
     for line in input_file:
         if line.startswith('>'):
             if id is not None:
@@ -22,4 +22,4 @@ with open('./'+sequence_file, 'r') as input_file, open('./'+sys.argv[2]+sys.argv
             seq.append(line.replace('\r\n', '').replace('\n', ''))
     sequence = ''.join(seq)
     id2seq_dict[id] = sequence
-    out.write('>'+target_name+'\n'+id2seq_dict[target_name]+'\n')
+    out.write('>'+target_name+'\n'+id2seq_dict[target_name][start_site:end_site + 1]+'\n')
