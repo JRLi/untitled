abd6bf87ee361f91e80aba7fd91e3df4e975ec11
JRLi <fanicesiza@gmail.com>
2017-04-24 17:24:26 +0800

    test

--- GDC/gdscWGS.py
+++ GDC/gdscWGS.py
@@ -1,10 +1,12 @@
 from collections import defaultdict
 
+frequency = 50
+
 def main():
     mut2cell_dict = defaultdict(list)
     cell_set = set()
     with open('D://Project/drs/gdsc/release_6.0/WES_variants.txt', 'r') as input_file, \
-            open('D://Project/drs/gdsc/release_6.0/gdsc_WES_df.txt', 'w') as output_file:
+            open('D://Project/drs/gdsc/release_6.0/gdsc_WES_df_' + str(frequency) + '.txt', 'w') as output_file:
         line_count = 0
         for line in input_file:
             line_count += 1
@@ -21,17 +23,16 @@ def main():
         output_file.write('\t' + '\t'.join(cell_list) + '\n')
 
         for mut_id in mut_list:
-            v_list = [mut_id]
             cell_check_list = mut2cell_dict[mut_id]
+            if len(set(cell_check_list)) < frequency:
+                continue
+            v_list = [mut_id]
             for cell in cell_list:
                 if cell in cell_check_list:
                     v_list.append('1')
                 else:
                     v_list.append('0')
             output_file.write('\t'.join(v_list) + '\n')
-
-
-
 
 if __name__ == '__main__':
     main()
