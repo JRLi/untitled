7c0580be323223b6fadc57ecc6d439fe03d90d11
JRLi <fanicesiza@gmail.com>
2017-06-22 17:25:24 +0800

    test

--- GDC/tcga/snvSummary.py
+++ GDC/tcga/snvSummary.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python3.6
 import os
 
+description_column = 2
 
-def file_processor(file_path):
+
+def file_processor(file_path, m_c):
     with open(file_path) as inputFile:
         snv_count = 0; more2 = 0; more3 = 0; more5 = 0; more10 = 0; patients = 0
         for line in inputFile:
             lf = line.strip().split(',')
             if line.startswith(','):
-                groups = ['-'.join(x.split('-')[:4]) for x in lf[4:]]
+                groups = ['-'.join(x.split('-')[:4]) for x in lf[m_c:]]
                 patients = len(groups)
             else:
                 snv_count += 1
                 check = 0
-                for x in lf[4:]:
+                for x in lf[m_c:]:
                     if x != '0':
                         check += 1
                 if check >= 2:
@@ -34,7 +36,7 @@ def main():
                          'more5\tmore5rate\tmore10\tmore10rate\n')
         for file in file_list:
             if file.startswith('snv_indel.extracted'):
-                patient, snv, m2, m3, m5, m10 = file_processor(file)
+                patient, snv, m2, m3, m5, m10 = file_processor(file, description_column)
                 print(patient, snv, m2, m3, m5, m10)
                 out_status.write('{}\t{}\t{}\t{}\t{:.3f}\t{}\t{:.4f}\t{}\t{:.5f}\t{}\t{:.8f}\n'.format(
                     file, patient, snv, m2, m2/snv, m3, m3/snv, m5, m5/snv, m10, m10/snv))
