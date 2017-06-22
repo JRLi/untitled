9c5f0a8615282a785509b0282e4c7c94b5e1088c
JRLi <fanicesiza@gmail.com>
2017-06-22 16:55:50 +0800

    test

---
+++ GDC/lincs/rmCol.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import pandas as pd
+import os
+
+fileCheck = 'GSE70138'
+
+
+def openDF(in_path, direct='f'):
+    fpath, fname = os.path.split(in_path)
+    fbase, fext = os.path.splitext(fname)
+    df = pd.read_csv(in_path, index_col=0) if fext == '.csv' else pd.read_table(in_path, index_col=0)
+    if direct == 't':
+        df = df.transpose()
+    return df, fbase
+
+
+def main():
+    linc_list = os.listdir('.')
+    for lincs in linc_list:
+        if lincs.startswith(fileCheck):
+            df1, df1_base = openDF(lincs)
+            print(df1_base, df1.shape)
+            cols = [c for c in df1.columns if 'ctl_' not in c]
+            df1 = df1[cols]
+            print('after remove ctl:', df1.shape)
+            df1.to_csv(df1_base + '.csv')
+
+if __name__ == '__main__':
+    main()
