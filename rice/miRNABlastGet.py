a3bab903ed218f1c03658712c5d902efde253e5d
JRLi <fanicesiza@gmail.com>
2017-08-22 17:46:07 +0800

    test

--- rice/miRNABlastGet.py
+++ rice/miRNABlastGet.py
@@ -3,6 +3,7 @@ import argparse
 import os
 import sys
 from collections import defaultdict
+import pandas as pd
 check = 'annot_0.0'
 blast_d = 'Blast_tmp'
 miRNA_d = 'forMiRNA'
@@ -36,8 +37,8 @@ def get_osa_dict(path):
 def unmatched_get(read_list, rp):
     r_set = set(read_list)
     all_c, out_c = 0, 0
-    with open(os.path.join(miRNA_d, rp + '.collapse_m.fa')) as in_fa, \
-            open(os.path.join(unm_d, rp + '.unmatched_collapse_m.fa'), 'w') as out_fa:
+    with open(os.path.join(miRNA_d, rp + '_collapse_m.fa')) as in_fa, \
+            open(os.path.join(unm_d, rp + '_unmatched_collapse_m.fa'), 'w') as out_fa:
         for line in in_fa:
             if line.startswith('>'):
                 all_c += 1
@@ -73,7 +74,8 @@ def blast_get(b_file, qc, tc, idt, rp):
 def get_exp(br_list, m2rl_dict, m2s_dict, rp, qc, tc, idt):
     with open(os.path.join(exp_d, '{}_{}_{}_{}_exp_wm.csv'.format(rp, qc, tc, idt)), 'w') as exp_wm, \
             open(os.path.join(exp_d, '{}_{}_{}_{}_exp_nm.csv').format(rp, qc, tc, idt), 'w') as exp_nm:
-        mir_count = 0
+        nm_dict, wm_dict = {}, {}
+        mir_count, nm_t, wm_t= 0, 0, 0
         for miRNA, r_list in m2rl_dict.items():
             mir_count += 1
             seq = m2s_dict.get(miRNA)
@@ -82,10 +84,13 @@ def get_exp(br_list, m2rl_dict, m2s_dict, rp, qc, tc, idt):
                 r_count = int(r_ID.split('-')[1])
                 count_nm += r_count
                 count_wm += (r_count / br_list.count(r_ID))
+            nm_t += count_nm
+            wm_t += count_wm
+            nm_dict[miRNA] = count_nm
+            wm_dict[miRNA] = count_wm
             exp_nm.write('{},{},{}\n'.format(miRNA, count_nm, seq))
             exp_wm.write('{},{},{}\n'.format(miRNA, count_wm, seq))
-            if mir_count <= 5:
-                print('[check]{}\t{}\t{}\t{}\t{}\t{}'.format(rp, miRNA, count_nm, count_wm, r_list, seq))
+        return nm_dict, wm_dict, mir_count, nm_t, wm_t
 
 
 def main(argv=None):
@@ -100,15 +105,28 @@ def main(argv=None):
         # Start to process blast results
         file_list = os.listdir(blast_d)
         file_count = 0
-        for file in file_list:
-            if file.endswith(check):
-                file_count += 1
-                rice_p = file.replace('.osa_mature.fa.w8e-3.annot_0.0', '')
-                b_read_list, mr2read_dict = blast_get(file, argv.query, argv.target, argv.identity, rice_p)
-                print('{}\tmir2ReadList_dict: {}\tb_reads_list: {}'.format(rice_p, len(mr2read_dict), len(b_read_list)))
-                unmatched_get(b_read_list, rice_p)  # get unmatched fa
-                # get exp file
-                get_exp(b_read_list, mr2read_dict, mirna2seq_dict, rice_p, argv.query, argv.target, argv.identity)
+        rice2m2edict_dict, rice2m2edict_dict_wm = {}, {}
+        with open('blast_result_summary.csv', 'w') as sum_f:
+            for file in file_list:
+                if file.endswith(check):
+                    file_count += 1
+                    rice_p = file.replace('.osa_mature.fa.w8e-3.annot_0.0', '')
+                    b_read_list, mr2read_dict = blast_get(file, argv.query, argv.target, argv.identity, rice_p)
+                    print('{}\tmir2read_dict: {}\tb_reads_list: {}'.format(rice_p, len(mr2read_dict), len(b_read_list)))
+                    unmatched_get(b_read_list, rice_p)  # get unmatched fa
+                    # get exp file
+                    exp_nm_dict, exp_wm_dict, mir_n, total_rc, total_rc_m = get_exp\
+                        (b_read_list, mr2read_dict, mirna2seq_dict, rice_p, argv.query, argv.target, argv.identity)
+                    sum_f.write('{},{},{},{}\n'.format(rice_p, mir_n, total_rc, total_rc_m))
+                    rice2m2edict_dict[rice_p] = exp_nm_dict
+                    rice2m2edict_dict_wm[rice_p] = exp_wm_dict
+        df_nm = pd.DataFrame(rice2m2edict_dict)
+        df_wm = pd.DataFrame(rice2m2edict_dict_wm)
+        df_nm = df_nm.fillna(0)
+        df_wm = df_wm.fillna(0)
+        print('df_nm.shape: {}\tdf_wm.shape: {}'.format(df_nm.shape, df_wm.shape))
+        df_nm.to_csv('blast_df_nm.csv')
+        df_wm.to_csv('blast_df_wm.csv')
         print('[Done]Processed file:{}'.format(file_count))
 
 if __name__ == '__main__':
